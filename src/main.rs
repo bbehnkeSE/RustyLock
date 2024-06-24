89c46733b0232d575973b8e93696a9e4c5eba526
Brian Behnke <bbehnke3644@gmail.com>
2024-06-24 02:13:13 -0400

    Beginning work on control flow

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,60 @@ use aes_gcm::{
     Aes256Gcm, Key, Nonce
 };
 
+use std::fs;
+use std::env;
+use std::path::Path;
+
+enum Control {
+    FileEnc,
+    FileDec,
+    DirEnc,
+    DirDec
+}
+
 fn main() {
-    let plaintext = "This is a test string".to_string();
+    let pth     = Path::new("./test/heart.png");
     let key_str = "thiskeystrmustbe32charlongtowork".to_string();
 
-    let encryped_data = encrypt(key_str.clone(), plaintext);
+    let con = Control::FileDec;
 
-    println!("Encrypted data: {:?}", encryped_data.clone());
+    match con {
+        Control::FileEnc => {
+            let data = fs::read(pth)
+                .expect("Unable to read file.");
+            let data = encrypt(&key_str, data);
+    
+            fs::write(pth, data)
+                .expect("Unable to write encrypted data to file.");
+        }
+        Control::FileDec => {
+            let data = String::from_utf8(fs::read(pth)
+                            .expect("Unable to read file."))
+                            .expect("Unable to convert data to String.");
+            
+            let data = decrypt(key_str, data);
+            fs::write(pth, data)
+                .expect("Unable to write decrypted data to file.");
+        }
+        _ => panic!("hsdf")
+    }
 
-    let decrypted_data = decrypt(key_str, encryped_data);
+    // if pth.is_file() {
+    //     let data = fs::read(pth)
+    //         .expect("Unable to read file.");
+    //     let enc = encrypt(&key_str, data);
 
-    println!("Decrypted data: {:?}", decrypted_data);
+    //     fs::write(pth, enc)
+    //         .expect("Unable to write file.");
+    // }
 }
 
 
-fn encrypt(key_str: String, plaintext: String) -> String {
+fn encrypt(key_str: &String, data: Vec<u8>) -> String {
     let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
     let cipher = Aes256Gcm::new(key);
-    let cipher_text = cipher.encrypt(&nonce, plaintext.as_bytes())
+    let cipher_text = cipher.encrypt(&nonce, &*data)
         .expect("failed to encrypt");
 
     let mut encryped_data: Vec<u8> = nonce.to_vec();
@@ -30,22 +65,16 @@ fn encrypt(key_str: String, plaintext: String) -> String {
     return hex::encode(encryped_data);
 }
 
-fn decrypt(key_str: String, encrypted_data: String) -> String {
+fn decrypt(key_str: String, encrypted_data: String) -> Vec<u8> {
     let encrypted_data = hex::decode(encrypted_data)
         .expect("failed to decode hex string into vec");
 
     let key = Key::<Aes256Gcm>::from_slice(key_str.as_bytes());
-
     let (nonce_arr, ciphered_data) = encrypted_data.split_at(12);
     let nonce = Nonce::from_slice(nonce_arr);
-
     let cipher = Aes256Gcm::new(key);
-
-    let plaintext = cipher.decrypt(nonce, ciphered_data)
+    let decrypted_data = cipher.decrypt(nonce, ciphered_data)
         .expect("failed to decrypt data");
 
-    String::from_utf8(plaintext)
-        .expect("failed to convert vector of bytes to string")
+    return decrypted_data;
 }
-
-
