1cde1e16cf94b2a9e47ccef72551b8fb5f352226
Brian Behnke <bbehnke3644@gmail.com>
2024-07-07 05:46:16 -0400

    Begin work on GUI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,59 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use std::env;
 use std::path::Path;
 
+use eframe::egui;
+
 mod locker;
+
+
+fn main() {
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 640.0])
+            .with_drag_and_drop(true),
+        ..Default::default()
+    };
+    eframe::run_native("RustyLock", native_options, Box::new(
+        |_cc| Ok(Box::<RustyLock>::default()))).expect("Uh oh...");
+
+    // // CLI Code
+    // let args: Vec<String> = env::args().collect();
+    // let mut opt: Options = { 
+    //     Options {
+    //         control: Control::Decrypt,
+    //         path: "".to_string()
+    //     }
+    // };
+    // for (i, arg) in args.iter().enumerate() {
+    //     if arg == "-e" {
+    //         opt.control = Control::Encrypt;
+    //     }
+    //     if arg == "-d" {
+    //         opt.control = Control::Decrypt;
+    //     }
+    //     if arg == "-p" {
+    //         opt.path = args[i + 1].clone();
+    //     }
+    // }
+
+    // let pth      = Path::new(&opt.path);
+    // let password = rpassword::prompt_password("Enter password: ").unwrap();
+    // let key      = locker::gen_key(password.as_bytes());
+
+    // match opt.control {
+    //     Control::Encrypt => {
+    //         locker::encrypt(&key, pth)
+    //             .expect("encrypt failed.");
+    //     }
+    //     Control::Decrypt => {
+    //         locker::decrypt(&key, pth)
+    //             .expect("decrypt failed.");
+    //     }
+    // }
+}
+
 
 #[derive(Debug)]
 enum Control {
@@ -16,38 +68,114 @@ struct Options {
     path:    String,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut opt: Options = { 
-        Options {
-            control: Control::Decrypt,
-            path: "".to_string()
-        }
-    };
-    for (i, arg) in args.iter().enumerate() {
-        if arg == "-e" {
-            opt.control = Control::Encrypt;
-        }
-        if arg == "-d" {
-            opt.control = Control::Decrypt;
-        }
-        if arg == "-p" {
-            opt.path = args[i + 1].clone();
-        }
+
+#[derive(Default)]
+struct RustyLock {
+    dropped_files: Vec<egui::DroppedFile>,
+    picked_paths: Vec<Option<String>>
+}
+
+impl RustyLock {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        return Self::default();
     }
+}
 
-    let pth      = Path::new(&opt.path);
-    let password = rpassword::prompt_password("Enter password: ").unwrap();
-    let key      = locker::gen_key(password.as_bytes());
+impl eframe::App for RustyLock {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Drag and drop files.");
 
-    match opt.control {
-        Control::Encrypt => {
-            locker::encrypt(&key, pth)
-                .expect("encrypt failed.");
-        }
-        Control::Decrypt => {
-            locker::decrypt(&key, pth)
-                .expect("decrypt failed.");
-        }
+            if ui.button("Browse").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.picked_paths.push(Some(path.display().to_string()));
+                }
+            }
+
+            for picked_path in &self.picked_paths {
+                if let Some(picked_path) = picked_path {
+                    ui.horizontal(|ui| {
+                        ui.label("Picked file:");
+                        ui.monospace(picked_path);
+                    });
+                }
+            }
+
+            if !self.dropped_files.is_empty() {
+                ui.group(|ui| {
+                    ui.label("Dropped files:");
+
+                    for file in &self.dropped_files {
+                        let mut info = if let Some(path) = &file.path {
+                            path.display().to_string()
+                        } else if !file.name.is_empty() {
+                            file.name.clone()
+                        } else {
+                            "???".to_owned()
+                        };
+
+                        let mut additional_info = vec![];
+                        if !file.mime.is_empty() {
+                            additional_info.push(format!("type {}", file.mime));
+                        }
+                        if let Some(bytes) = &file.bytes {
+                            additional_info.push(format!("{} bytes", bytes.len()));
+                        }
+                        if !additional_info.is_empty() {
+                            info += &format!(" ({})", additional_info.join(", "));
+                        }
+
+                        ui.label(info);
+                    }
+                });
+            }
+        });
+
+        preview_files_being_dropped(ctx);
+
+        // Collect dropped files
+        ctx.input(|i| {
+            if !i.raw.dropped_files.is_empty() {
+                self.dropped_files.clone_from(&i.raw.dropped_files);
+            }
+        })
+    }
+}
+
+
+fn preview_files_being_dropped(ctx: &egui::Context) {
+    use egui::*;
+    use std::fmt::Write as _;
+
+    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
+        let text = ctx.input(|i| {
+            let mut text = "Dropping files:\n".to_owned();
+            for file in &i.raw.hovered_files {
+                if let Some(path) = &file.path {
+                    write!(text, "\n{}", path.display()).ok();
+                } else if !file.mime.is_empty() {
+                    write!(text, "\n{}", file.mime).ok();
+                } else {
+                    text += "\n???";
+                }
+            }
+            text
+        });
+
+        let painter = ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
+
+        let screen_rect = ctx.screen_rect();
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+        painter.text(
+            screen_rect.center(),
+            Align2::CENTER_CENTER,
+            text,
+            TextStyle::Heading.resolve(&ctx.style()),
+            Color32::WHITE
+        );
     }
 }
